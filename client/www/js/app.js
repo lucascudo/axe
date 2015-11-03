8ff107ef3175db6efdfab6349b98b019eedd9bb9
Lucas Teixeira Rocha <lucas.sisau@gmail.com>
2015-11-03 10:54:27 +0000

    Big bang

---
+++ client/www/js/app.js
@@ -0,0 +1,25 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform, $rootScope) {
+	
+	$rootScope.goOutside = function (url, target) {
+		target = target || "_system";
+		window.open(url, target);
+	};
+	
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if(window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if(window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
+})
