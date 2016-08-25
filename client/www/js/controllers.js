dd4d93af3db1228a102a5dbcff0e0afc8ec2ecbb
Lucas Rocha <lucas.rocha@makerplanet.com>
2016-08-25 13:38:48 -0300

    Correção no player de audios

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,36 +1,38 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function ($rootScope) {
+.controller('HomeCtrl', function HomeCtrl($rootScope) {
 	$rootScope.themeColor = "positive";
 })
 
-.controller('DictionaryCtrl', function ($rootScope, $scope, ionicMaterialMotion, DictionaryFactory) {
+.controller('DictionaryCtrl', function DictionaryCtrl($rootScope, $scope, ionicMaterialMotion, DictionaryFactory) {
 	$rootScope.themeColor = "assertive";
-	$scope.toggleGroup = function (group) {
+	$scope.toggleGroup = function toggleGroup(group) {
 		if ($scope.isGroupShown(group)) {
 		  $scope.shownGroup = null;
 		} else {
 		  $scope.shownGroup = group;
 		}
 	};
-	$scope.isGroupShown = function (group) {
+	$scope.isGroupShown = function isGroupShown(group) {
 		return $scope.shownGroup === group;
 	};
 	$scope.dictionary = DictionaryFactory;
 	ionicMaterialMotion.ripple();
 })
 
-.controller('MessageCtrl', function ($rootScope, $scope, ḾessageFactory, MediaSrv) {
-	$rootScope.themeColor = "energized";
-	$scope.message = ḾessageFactory;
-	MediaSrv.loadMedia('audio/' + $scope.message.filename, null, null, function onStop() {
+.controller('MessageCtrl', function MessageCtrl($rootScope, $scope, ḾessageFactory, MediaSrv) {
+	function onMediaStop() {
 		document.getElementById("btn-pause").classList.add("hide");
 		document.getElementById("btn-play").classList.remove("hide");
-	}).then(function(media){
-		$scope.message.media = media;
-	});
+	}
+	function onMediaSuccess(media) { $scope.message.media = media; }
+	function onMediaError(err) { console.log(err); }
+	$rootScope.themeColor = "energized";
+	$scope.message = ḾessageFactory;
+	
+	MediaSrv.loadMedia('audio/mae/' + $scope.message.filename, null, null, onMediaStop).then(onMediaSuccess, onMediaError);
 })
 
-.controller('AboutCtrl', function ($rootScope) {
+.controller('AboutCtrl', function AboutCtrl($rootScope) {
 	$rootScope.themeColor = "balanced";
 });
